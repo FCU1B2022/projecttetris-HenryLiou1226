b1400c4f61c60ebabc93717c26164a88ba0e61ca
Henry <henry.liou2003@gmail.com>
2023-04-27 10:29:38 +0800

    4/27

--- projectTetris-HenryLiou1226/main.c
+++ projectTetris-HenryLiou1226/main.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 typedef enum
 {
     RED = 41,
@@ -26,5 +27,73 @@ typedef struct
 {
     Color color;
     ShapeId shape;
+    int size;
+    char rotates[4][4][4];
+}Shape;
+typedef struct 
+{
+    Color color;
+    ShapeId shape;
 }Block;
+Shape shape[7] = {
+    {
+        .shape = I,
+        .color = CYAN,
+        .size = 4,
+        .rotates = 
+        {
+            {
+                {0,0,0,0},
+                {1,1,1,1},
+                {0,0,0,0},
+                {0,0,0,0},
+            },
+            {
+                {0,0,1,0},
+                {0,0,1,0},
+                {0,0,1,0},
+                {0,0,1,0},
+            },
+            {
+                {0,0,0,0},
+                {0,0,0,0},
+                {1,1,1,1},
+                {0,0,0,0},
+            },
+            {
+                {0,1,0,0},
+                {0,1,0,0},
+                {0,1,0,0},
+                {0,1,0,0},
+            },
+        }
+    },
+};
 
+int main()
+{
+    Color cur;
+    for (int i = 0;i < 7;i++)
+    {
+        for(int r = 0;r < 4;r++)
+        {
+            for(int s = 0;s < shape[i].size;s++)
+            {
+                for(int t= 0;t < shape[i].size;t++)
+                {
+                    if(shape[i].rotates[r][s][t])
+                    {
+                        cur = shape[i].color;
+                    }
+                    else
+                    {
+                        cur = BLACK;
+                    }
+                    printf("\033[%dm  \033[0m",cur);
+                }
+                printf("\n");
+            }
+        }
+    }
+    return 0;
+}
