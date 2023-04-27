f6fa99ec9ab04ddea235fea64d28539f2cd12cf8
Henry <henry.liou2003@gmail.com>
2023-04-27 09:58:52 +0800

    4/27

--- projectTetris-HenryLiou1226/main.c
+++ projectTetris-HenryLiou1226/main.c
@@ -1,6 +1,30 @@
-#include<stdio.h>
-int main()
+typedef enum
 {
-    printf("Hello World!\n");
-    return 0;
-}
+    RED = 41,
+    GREEN,
+    YELLOW,
+    BLUE,
+    PURPLE,
+    CYAN,
+    WHITE,
+    BLACK = 0   ,
+}Color ;
+
+typedef enum
+{
+    EMPTY = -1,
+    I,
+    J,
+    L,
+    O,
+    S,
+    T,
+    Z
+}ShapeId;
+
+typedef struct 
+{
+    Color color;
+    ShapeId shape;
+}Block;
+
